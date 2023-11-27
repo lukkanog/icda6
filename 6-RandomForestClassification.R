43c31f7bdbbad7ce146b03886638d6276f0a0042
Henrique Souza <oliveira.henrique2@aluno.ifsp.edu.br>
2023-11-26 22:14:18 -0300

    feat: running rf with all entire training dataset

--- 6-RandomForestClassification.R
+++ 6-RandomForestClassification.R
@@ -1,29 +1,62 @@
 library(randomForest)
 library(pROC)
+library(caret)
+library(ggplot2)
 
 set.seed(21)
 
-head_df_treinamento <- head(DadosGravidez2021Treinamento, 10000)
+#head_df_treinamento <- head(DadosGravidez2021Treinamento, 500000)
 
-head_df_teste <- head(DadosGravidez2021Teste, 10000)
+# head_df_treinamento <- DadosGravidez2021Treinamento
+
+# head_df_teste <- head(DadosGravidez2021Teste, 500000)
+
+glimpse(DadosGravidez2021Treinamento)
+
+glimpse(DadosGravidez2021Teste)
 
 rf <-
   randomForest(
     formula =  PARTO ~ ESCMAE + RACACORMAE + LOCNASC + FAIXAETARIA + UF + GRAVIDEZ + PARIDADE + JA_TEVE_PARTO_CESARIA + JA_TEVE_PARTO_VAGINAL,
-    data = head_df_treinamento,
-    importance = TRUE,
+    data = DadosGravidez2021Treinamento,
+    importance = FALSE,
     # proximity = TRUE,
-    maxnodes=1000
+    ntree=1000,
+    maxnodes=10
   )
 
-prediction <- predict(rf, head_df_teste)
+prediction <- predict(rf, DadosGravidez2021Teste)
 
-head_df_teste$CORRECT_PREDICTION <- prediction
-head_df_teste$CORRECT_PREDICTION <- head_df_teste$PARTO == head_df_teste$CORRECT_PREDICTION
+# DadosGravidez2021Teste$CORRECT_PREDICTION <- prediction
+# DadosGravidez2021Teste$CORRECT_PREDICTION <- head_df_teste$PARTO == DadosGravidez2021Teste$CORRECT_PREDICTION
+# 
+# summary(DadosGravidez2021Teste$CORRECT_PREDICTION)
 
-summary(head_df_teste$CORRECT_PREDICTION)
+# DadosGravidez2021Teste$CORRECT_PREDICTION <- NULL
 
-View(head(head_df_teste))
+View(head(DadosGravidez2021Teste))
 
-cm <- confusionMatrix(data=prediction, reference = head_df_teste$PARTO)
+str(DadosGravidez2021Teste)
+
+str(DadosGravidez2021Teste$PARTO)
+
+cm <- confusionMatrix(data=prediction, reference = DadosGravidez2021Teste$PARTO)
 cm
+
+
+ArvoreGravidezTreinamento.class.pred_6 <- predict(rf, DadosGravidez2021Teste, type = "prob")[, 2]
+ArvoreGravidezTreinamento.class_roc_6<-roc(DadosGravidez2021Teste$PARTO ~ ArvoreGravidezTreinamento.class.pred_6, plot = TRUE, print.auc = TRUE)
+
+
+# df_1000 = head(DadosGravidez2021Teste, 1000)
+
+# features <- setdiff(names(DadosGravidez2021Treinamento), "PARTO")
+# m2 <- tuneRF(
+#   x          = DadosGravidez2021Treinamento[features],
+#   y          = DadosGravidez2021Treinamento$PARTO,
+#   ntreeTry   = 500,
+#   mtryStart  = 5,
+#   stepFactor = 1.5,
+#   improve    = 0.01,
+#   trace      = TRUE     
+# )
